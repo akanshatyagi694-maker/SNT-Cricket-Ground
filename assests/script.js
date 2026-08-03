c3a6573f61a82e3ae5795da4fd857787827e1e8d
Akansha Tyagi <akanshatyagi694@gmail.com>
2026-08-03 21:41:40 +0530

    Premium website redesign

--- assests/script.js
+++ assests/script.js
@@ -112,7 +112,7 @@ dateInput.addEventListener("change", async () => {
 });
         try {
 
-            const response = await fetch("https://snt-cricket-ground-production.up.railway.app", {
+            const response = await fetch("http://127.0.0.1:5000/api/bookings", {
 
                 method: "POST",
 
